6a35221a92a249f7a65933744de0dd64a87ccfb1
beatrizdaddea <beatrizchith@gmail.com>
2023-07-11 15:41:02 -0300

    status_http

--- alura-books-server/rotas/livro.js
+++ alura-books-server/rotas/livro.js
@@ -1,9 +1,22 @@
+// rotas > livro.js
+
 const { Router } = require("express")
+const { getLivros } = require("../controladores/livro")
 
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.send('Olá mundo da Alura do Brasil!')
+router.get('/', getLivros)
+
+router.post('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo POST')
+})
+
+router.patch('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo PATCH')
+})
+
+router.delete('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo DELETE')
 })
 
 module.exports = router
