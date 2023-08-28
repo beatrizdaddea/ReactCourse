4a737b79434d29f0f70e942ff174cd0b0afa3abe
beatrizdaddea <beatrizchith@gmail.com>
2023-08-28 15:07:53 -0300

    add routes

--- alura-books/src/index.js
+++ alura-books/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Home from './rotas/Home';
 import reportWebVitals from './reportWebVitals';
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './componentes/Header';
+import Favoritos from './rotas/Favoritos';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -20,7 +23,7 @@ const GlobalStyle = createGlobalStyle`
   }
 
   li {
-    list-style: none;    
+    list-style: none;
   }
 
 
@@ -29,12 +32,15 @@ const GlobalStyle = createGlobalStyle`
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
-  </React.StrictMode>
+  <GlobalStyle />
+  <BrowserRouter>
+  <Header />
+      <Routes>
+          <Route path="/favoritos" element={<Favoritos />} />
+          <Route path="/" element={<Home />} />
+      </Routes>
+  </BrowserRouter>
+</React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
